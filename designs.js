08df7bf8f2146d1e23badb5eabd2310fdf230f97
amirabuhasan <amirabuhasan@gmail.com>
2017-09-10 23:39:07 +0800

    Completed Project
    - Separated functions chooseColor() and useGrid functions on on JS.
    - Added comments to JS.
    - Added pointer when hovering pixel_canvas to CSS.

--- designs.js
+++ designs.js
@@ -1,30 +1,33 @@
-// Select color input
-// Select size input
+// Gets color code from colorPicker, and adds it to pixel clicked.
+function chooseColor() {
+  $("#pixel_canvas").on("click", "td", function(){
+    let color = $("#colorPicker").val();
+    $(this).css("background-color", color);
+  });
+};
 
-// When size is submitted by the user, call makeGrid()
+// Creates a grid depending on the height and width a user selected.
+function makeGrid(height,width) {
+  for (let h = 0; h < height; h++){
+    $("#pixel_canvas").append("<tr></tr>")
+  };
+  for (let w = 0; w < width; w++){
+    $("tr").append( `<td></td>`)
+  };
+};
 
-function makeGrid() {
+/* On clicking "Submit", gets a users selected height and width and calls makeGrid().
+After that, calls chooseColor(). */
+function useGrid() {
   $("#submit").click(function(event) {
     event.preventDefault()
     $("#pixel_canvas").empty();
     let height = $("#input_height").val();
     let width = $("#input_width").val();
-    $("#input_height").val(height);
-    $("#input_width").val(width);
-    for (let h = 0; h < height; h++){
-      $("#pixel_canvas").append("<tr></tr>")
-    };
-    for (let w = 0; w < width; w++){
-      $("tr").append( `<td id=${w}></td>`)
-    };
+    $(makeGrid(height,width));
   });
-  //add color
-  $("#pixel_canvas").on("click", "td", function(){
-    let color = $("#colorPicker").val();
-    $(this).css("background-color", color);
-  });
-
-}
+  $(chooseColor());
+};
 
 
-$(makeGrid());
+$(useGrid());
