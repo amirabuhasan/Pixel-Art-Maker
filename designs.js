f7c7c95a357da5d863225f5c4ab818e5eaf17e5f
amirabuhasan <amirabuhasan@gmail.com>
2017-09-10 22:28:39 +0800

    Added function to create rows and columns dynamically

--- designs.js
+++ designs.js
@@ -3,8 +3,31 @@
 
 // When size is submitted by the user, call makeGrid()
 
+
+
 function makeGrid() {
+  $("#submit").click(function(event) {
+    event.preventDefault()
+    let height = $("#input_height").val();
+    let width = $("#input_width").val();
+    $("#input_height").val(height);
+    $("#input_width").val(width);
+    console.log(height);
+    for (let h = 0; h < height; h++){
+      $("#pixel_canvas").append( "<tr></tr>" )
+    }
+    for (let w = 0; w < width; w++){
+      $("tr").append( "<td></td>" )
+    }
+  })
+
+
+
+//
 
 // Your code goes here!
 
 }
+
+
+$(makeGrid());
